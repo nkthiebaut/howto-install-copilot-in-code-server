c464f0a81a34105d866ef1d12e5024ca42cde399
Nicolas <10967508+nkthiebaut@users.noreply.github.com>
2025-11-19 14:28:30 -0800

    Fix version extraction

--- install-copilot.sh
+++ install-copilot.sh
@@ -2,7 +2,7 @@
 
 # Extract VS Code version from code-server
 get_vscode_version() {
-    code-server --version | head -n1
+    code-server --version | head -n1 | cut -d' ' -f1
 }
 
 # Get user-data-dir from running code-server process
